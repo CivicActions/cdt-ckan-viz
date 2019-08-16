ab42910e033badc06e95624835f4bbe6c7c7b2da
Tom Wood <tom.wood@civicactions.com>
2019-08-16 11:07:48 -0400

    add a few more widgets

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 var config = {
- widgets: [
+  widgets: [
     {
       "elementId": "unemployment_state_average",
       "view": {
         "metadata": {},
         "resources": [
           {
-              "datasetId": "local_area_unemployment_statistics",
+            "datasetId": "local_area_unemployment_statistics",
             "name": "local_area_unemployment_statistics__laus___annual_average",
             "transform": [
-                {
-                    "type": "filter",
-                    "expression": "data['Area Type'] == 'State'"
-                }
+              {
+                "type": "filter",
+                "expression": "data['Area Type'] == 'State'"
+              }
             ]
           }
         ],
@@ -20,15 +20,25 @@ var config = {
         "spec": {
           "$schema": "https://vega.github.io/schema/vega/v3.json",
           "height": 160,
-          "width": 350,
+          "width": 238,
           "padding": 0,
+          "data": [
+            {
+              "name": "local_area_unemployment_statistics__laus___annual_average",
+              "format": {
+                "parse": {
+                    "Year": "date"
+                }
+              }
+            }
+          ],
           "scales": [
             {
               "name": "x",
               "type": "utc",
               "range": "width",
               "domain": {
-                  "data": "local_area_unemployment_statistics__laus___annual_average",
+                "data": "local_area_unemployment_statistics__laus___annual_average",
                 "field": "Year"
               }
             },
@@ -39,7 +49,7 @@ var config = {
               "nice": true,
               "zero": false,
               "domain": {
-                  "data": "local_area_unemployment_statistics__laus___annual_average",
+                "data": "local_area_unemployment_statistics__laus___annual_average",
                 "field": "Unemployment Rate"
               }
             }
@@ -101,13 +111,13 @@ var config = {
         "metadata": {},
         "resources": [
           {
-              "datasetId": "local_area_unemployment_statistics",
+            "datasetId": "local_area_unemployment_statistics",
             "name": "local_area_unemployment_statistics__laus___annual_average",
             "transform": [
-                {
-                    "type": "filter",
-                    "expression": "(data['Area Type'] == 'County') && (data['Area Name'] == 'Santa Clara County')"
-                }
+              {
+                "type": "filter",
+                "expression": "(data['Area Type'] == 'County') && (data['Area Name'] == 'Santa Clara County')"
+              }
             ]
           }
         ],
@@ -115,15 +125,25 @@ var config = {
         "spec": {
           "$schema": "https://vega.github.io/schema/vega/v3.json",
           "height": 160,
-          "width": 350,
+          "width": 238,
           "padding": 0,
+          "data": [
+            {
+              "name": "local_area_unemployment_statistics__laus___annual_average",
+              "format": {
+                "parse": {
+                    "Year": "date"
+                }
+              }
+            }
+          ],
           "scales": [
             {
               "name": "x",
               "type": "utc",
               "range": "width",
               "domain": {
-                  "data": "local_area_unemployment_statistics__laus___annual_average",
+                "data": "local_area_unemployment_statistics__laus___annual_average",
                 "field": "Year"
               }
             },
@@ -134,7 +154,217 @@ var config = {
               "nice": true,
               "zero": false,
               "domain": {
-                  "data": "local_area_unemployment_statistics__laus___annual_average",
+                "data": "local_area_unemployment_statistics__laus___annual_average",
+                "field": "Unemployment Rate"
+              }
+            }
+          ],
+          "axes": [
+            {
+              "orient": "bottom",
+              "scale": "x",
+              "labelFont": "Lato",
+              "format": "%Y",
+              "domain": false,
+              "ticks": false,
+              "labelPadding": 10,
+              "labelOverlap": true,
+              "labelFontSize": 9
+            },
+            {
+              "orient": "right",
+              "scale": "y",
+              "labelFont": "Lato",
+              "domain": false,
+              "ticks": false,
+              "grid": true,
+              "labelFontSize": 9
+            }
+          ],
+          "marks": [
+            {
+              "type": "line",
+              "from": {
+                "data": "local_area_unemployment_statistics__laus___annual_average"
+              },
+              "encode": {
+                "enter": {
+                  "x": {
+                    "scale": "x",
+                    "field": "Year"
+                  },
+                  "y": {
+                    "scale": "y",
+                    "field": "Unemployment Rate"
+                  },
+                  "strokeWidth": {
+                    "value": 2
+                  },
+                  "stroke": {
+                    "value": "#A95F6D"
+                  }
+                }
+              }
+            }
+          ]
+        }
+      }
+    },
+    {
+      "elementId": "unemployment_alameda_average",
+      "view": {
+        "metadata": {},
+        "resources": [
+          {
+            "datasetId": "local_area_unemployment_statistics",
+            "name": "local_area_unemployment_statistics__laus___annual_average",
+            "transform": [
+              {
+                "type": "filter",
+                "expression": "(data['Area Type'] == 'County') && (data['Area Name'] == 'Alameda County')"
+              }
+            ]
+          }
+        ],
+        "specType": "vega",
+        "spec": {
+          "$schema": "https://vega.github.io/schema/vega/v3.json",
+          "height": 160,
+          "width": 238,
+          "padding": 0,
+          "data": [
+            {
+              "name": "local_area_unemployment_statistics__laus___annual_average",
+              "format": {
+                "parse": {
+                    "Year": "date"
+                }
+              }
+            }
+          ],
+          "scales": [
+            {
+              "name": "x",
+              "type": "utc",
+              "range": "width",
+              "domain": {
+                "data": "local_area_unemployment_statistics__laus___annual_average",
+                "field": "Year"
+              }
+            },
+            {
+              "name": "y",
+              "type": "linear",
+              "range": "height",
+              "nice": true,
+              "zero": false,
+              "domain": {
+                "data": "local_area_unemployment_statistics__laus___annual_average",
+                "field": "Unemployment Rate"
+              }
+            }
+          ],
+          "axes": [
+            {
+              "orient": "bottom",
+              "scale": "x",
+              "labelFont": "Lato",
+              "format": "%Y",
+              "domain": false,
+              "ticks": false,
+              "labelPadding": 10,
+              "labelOverlap": true,
+              "labelFontSize": 9
+            },
+            {
+              "orient": "right",
+              "scale": "y",
+              "labelFont": "Lato",
+              "domain": false,
+              "ticks": false,
+              "grid": true,
+              "labelFontSize": 9
+            }
+          ],
+          "marks": [
+            {
+              "type": "line",
+              "from": {
+                "data": "local_area_unemployment_statistics__laus___annual_average"
+              },
+              "encode": {
+                "enter": {
+                  "x": {
+                    "scale": "x",
+                    "field": "Year"
+                  },
+                  "y": {
+                    "scale": "y",
+                    "field": "Unemployment Rate"
+                  },
+                  "strokeWidth": {
+                    "value": 2
+                  },
+                  "stroke": {
+                    "value": "#A95F6D"
+                  }
+                }
+              }
+            }
+          ]
+        }
+      }
+    },
+    {
+      "elementId": "unemployment_santa_cruz_average",
+      "view": {
+        "metadata": {},
+        "resources": [
+          {
+            "datasetId": "local_area_unemployment_statistics",
+            "name": "local_area_unemployment_statistics__laus___annual_average",
+            "transform": [
+              {
+                "type": "filter",
+                "expression": "(data['Area Type'] == 'County') && (data['Area Name'] == 'Santa Cruz County')"
+              }
+            ]
+          }
+        ],
+        "specType": "vega",
+        "spec": {
+          "$schema": "https://vega.github.io/schema/vega/v3.json",
+          "height": 160,
+          "width": 238,
+          "padding": 0,
+          "data": [
+            {
+              "name": "local_area_unemployment_statistics__laus___annual_average",
+              "format": {
+                "parse": {
+                    "Year": "date"
+                }
+              }
+            }
+          ],
+          "scales": [
+            {
+              "name": "x",
+              "type": "utc",
+              "range": "width",
+              "domain": {
+                "data": "local_area_unemployment_statistics__laus___annual_average",
+                "field": "Year"
+              }
+            },
+            {
+              "name": "y",
+              "type": "linear",
+              "range": "height",
+              "nice": true,
+              "zero": false,
+              "domain": {
+                "data": "local_area_unemployment_statistics__laus___annual_average",
                 "field": "Unemployment Rate"
               }
             }
@@ -191,5 +421,5 @@ var config = {
       }
     }
   ],
-    datasets: ["https://datahub.io/tom.wood/local_area_unemployment_statistics"]
+  datasets: ["https://datahub.io/tom.wood/local_area_unemployment_statistics"]
 }
